59bd4597e7228caee12ab046f1ba43ad49705204
Arunsoni9754 <111188653+Arunsoni9754@users.noreply.github.com>
2022-09-14 21:46:06 +0530

    Create Q6.c

---
+++ Q6.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void natural(int);
+int main()
+{
+    int n;
+    printf("Enter the number of terms ");
+    scanf("%d",&n);
+    natural(n);
+    return 0;
+}
+void natural(int i)
+{
+    if(i>0)
+    {
+        if(i%2==0)
+        printf("%d ",i);
+                natural(i-1);
+
+    }
+}
